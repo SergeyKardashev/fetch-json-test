6470bbe49be9a7db7f7c2a4a26d7101260c5a58a
SergeyKardashev <angeldeep@yandex.ru>
2024-07-05 22:12:32 +0700

    feat: add function that fetches sales data, and makes table headers.

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
-async function asyncFetch() {
-  const response = await fetch("./data.json");
+async function asyncFetchPerson() {
+  const response = await fetch("./person.json");
   console.log("ответ получен", response);
   const readableData = await response.json();
   console.log("ответ распарсен", readableData);
   return readableData;
 }
 
-async function renderData() {
-  const fetchedData = await asyncFetch();
-  console.log("fetchedData", fetchedData);
-  console.log(fetchedData.job);
+async function consolePerson() {
+  const fetchedPerson = await asyncFetchPerson();
+  console.log("fetchedData", fetchedPerson);
+  console.log(fetchedPerson.job);
 }
 
-renderData();
+consolePerson();
+
+fetchParamsSales = { url: "./sales.json" };
+
+async function fetchData(params) {
+  const rawData = await fetch(params.url);
+  const data = await rawData.json();
+  console.log("fetch function return ", data);
+  return data;
+}
+
+async function renderSales() {
+  const salesData = await fetchData(fetchParamsSales);
+  console.log("salesData ", salesData);
+  // нахожу контейнер для таблицы
+  const tableContainer = document.querySelector(".table-wrap");
+
+  // создаю таблицу в переменную, а не в дом
+  const mainTable = document.createElement("table");
+  mainTable.classList.add("main-table");
+
+  // создаю шапку таблицы
+  const tableHeader = document.createElement("thead");
+
+  // создаю строку для шапки
+  const headerRow = document.createElement("tr");
+
+  // наполняю строку шапки данными из json
+  salesData.headers.forEach((item) => {
+    const td = document.createElement("td");
+    td.textContent = item;
+    headerRow.append(td);
+  });
+
+  // вставляю строку заголовка в шапку
+  tableHeader.append(headerRow);
+
+  // вставляю шапку в таблицу
+  mainTable.append(tableHeader);
+
+  // вставляю таблицу контейнер для таблицы (в ДОМ)
+  tableContainer.append(mainTable);
+}
+
+renderSales();
